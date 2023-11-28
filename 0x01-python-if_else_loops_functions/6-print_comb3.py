5ae318e3c74f9d538362673f318f90515c4f2178
Paul Ogolla <paulotieno2@gmail.com>
2023-11-29 01:16:25 +0300

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for a in range(10):
     for b in range(10):
         if a < b:
-            print("{}{}".format(a,b), end='')
+            print("{}{}".format(a, b), end='')
 
             if a < 8:
                 print(", ", end='')
