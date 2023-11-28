3aff8ef2efe90c53d2cced94cafda90ea7a38269
Paul Ogolla <paulotieno2@gmail.com>
2023-11-29 01:15:30 +0300

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for a in range(10):
+    for b in range(10):
+        if a < b:
+            print("{}{}".format(a,b), end='')
+
+            if a < 8:
+                print(", ", end='')
+print()
