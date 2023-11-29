b8c385b24e04ae228d0349e6102f2dbb914bfd4a
Paul Ogolla <paulotieno2@gmail.com>
2023-11-29 23:40:34 +0300

    fix tabs

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in reversed(range(97, 123)):
 	if a % 2 != 0:
-		print("{:c}".format(a - 32), end='')
-	else:
-		print("{:c}".format(a), end='')
+        print("{:c}".format(a - 32), end='')
+    else:
+        print("{:c}".format(a), end='')
