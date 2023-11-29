6906cb62efbb5ed68af00c1ea5622ec3c833cc56
Paul Ogolla <paulotieno2@gmail.com>
2023-11-29 11:26:53 +0300

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     up = []
     for s in str:
-        if ord(i) >= 97 and ord(str) <= 122:
+        if ord(s) >= 97 and ord(s) <= 122:
             up.append(chr(ord(s) - 32))
         else:
             up.append(s)
