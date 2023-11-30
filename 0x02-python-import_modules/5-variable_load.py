a2aa16fabf96ab31076a8501afa1afed40ae5804
Paul Ogolla <paulotieno2@gmail.com>
2023-12-01 00:37:01 +0300

    task

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python3
-from variable_load_5 import a
-
-
-def main():
-    print("{}".format(a))
+#!/usr/bin/python3
 
 
 if __name__ == "__main__":
-    main()
+    from variable_load_5 import a
+    print("{}".format(a))
