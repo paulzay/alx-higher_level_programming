4ff5e7efb366f0ec3de50c5c1e21707f67183c02
Paul Ogolla <paulotieno2@gmail.com>
2023-12-01 01:07:37 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 
-def main():
+if __name__ == "__main__":
     module_names = dir('4-hidden_discovery.py')
     for s in module_names:
         if not s.startswith("__"):
             print("{}".format(s))
-
-
-if __name__ == "__main__":
-    main()
