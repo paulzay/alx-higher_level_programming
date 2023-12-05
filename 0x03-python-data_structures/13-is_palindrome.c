0842ac8d8621c235e36719b3e58d7546a38ec393
Paul Ogolla <paulotieno2@gmail.com>
2023-12-05 23:52:54 +0300

    init

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - fn to check if palindromes
+ * @head: points to the head
+ * Description: is it palindromic
+ * Return: 0 or 1
+ */
+int is_palindrom(listint_t, **head)
+{
+	listint_t *tail;
+	listint_t *currentNode = *head;
+
+	if (*head == NULL)
+		return (1);
+	/*
+	while (currentNode != NULL)
+	{
+
+	}
+	*/
+}
