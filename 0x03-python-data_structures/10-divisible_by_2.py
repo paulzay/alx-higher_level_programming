f0fff3af453e9ba6fc78c72331875750d5d236cd
Paul Ogolla <paulotieno2@gmail.com>
2023-12-05 05:55:35 +0300

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,12 +2,12 @@
 def divisible_by_2(my_list=[]):
     nums_list = []
 
-    if not my_list:
-        return None
-    else:
+    if my_list:
         for num in my_list:
             if num % 2 == 0:
                 nums_list.append(num)
             else:
                 nums_list.append(False)
         return nums_list
+    else:
+        return None
