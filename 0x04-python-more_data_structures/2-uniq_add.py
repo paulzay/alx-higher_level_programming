7964357275ddec16883ed818e583a9900fbd3289
Paul Ogolla <paulotieno2@gmail.com>
2023-12-06 21:40:59 +0300

    2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+def uniq_add(my_list=[]):
+    total = 0
+    nums = []
+
+    for num in my_list:
+        if num in nums:
+            continue
+        else:
+            nums.append(num)
+
+    for i in nums:
+        total += i
+
+    return total
