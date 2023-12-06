a50733a2cfb9e877e6c10da66bc485f33c6e14f9
Paul Ogolla <paulotieno2@gmail.com>
2023-12-06 21:25:58 +0300

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+def search_replace(my_list, search, replace):
+    count = 0
+    for i in my_list:
+        count += 1
+        if i == search:
+            my_list[count - 1] = replace
+    return my_list
