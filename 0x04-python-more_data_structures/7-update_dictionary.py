5ecb2784335b5e12b5c8e5b13cb0a8aff9f08688
Paul Ogolla <paulotieno2@gmail.com>
2023-12-07 04:03:56 +0300

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-	if key not in a_dictionary.keys():
-		a_dictionary[key] = value
-	else:
-		for k in a_dictionary.keys():				
-			if k == key:
-				a_dictionary.update({key: value})
-
-	return a_dictionary
+    if key not in a_dictionary.keys():
+        a_dictionary[key] = value
+    else:
+        for k in a_dictionary.keys():
+            if k == key:
+                a_dictionary.update({key: value})
+    return a_dictionary
