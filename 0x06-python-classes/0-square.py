25369aaaa1478f1974a6ba31658a085119ff4c80
Paul Ogolla <paulotieno2@gmail.com>
2023-12-20 00:00:38 +0300

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square(object):
+    """
+    class Square
+
+    """
+    def __init__(self):
+        """
+        __init__ method with no args
+        """
+        pass
