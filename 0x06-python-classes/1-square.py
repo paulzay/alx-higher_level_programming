74fe58f88aabb771719ec6946f50d71330f9e0cf
Paul Ogolla <paulotieno2@gmail.com>
2023-12-20 01:19:47 +0300

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,14 +5,14 @@ class that defines a squaare
 
 
 class Square(object):
-	"""
-	class that defines a squaare
-	"""
-	def __init__(self, size):
-		"""
-	    initialize the class variables
-	    """
-	    """
-	    Args: arguments
-	    """
-		self.__size = size
+    """
+    class that defines a squaare
+    """
+    def __init__(self, size):
+        """
+        initialize the class variables
+        """
+        """
+        Args: arguments
+        """
+        self.__size = size
