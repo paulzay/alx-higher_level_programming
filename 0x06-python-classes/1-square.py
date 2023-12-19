f096d32449e99e740ad305656a10ad3863732a87
Paul Ogolla <paulotieno2@gmail.com>
2023-12-20 00:57:39 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class that defines a squaare
+"""
+
+
+class Square(object):
+	"""
+	class that defines a squaare
+	"""
+	def __init__(self, size):
+		"""
+	    initialize the class variables
+	    """
+	    """
+	    Args: arguments
+	    """
+		self.__size = size
