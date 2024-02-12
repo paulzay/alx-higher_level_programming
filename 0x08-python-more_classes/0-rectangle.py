51195c2c00adfa5052b16814baa6639ed3558782
Paul Ogolla <paulotieno2@gmail.com>
2024-02-13 00:11:21 +0300

    documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+class that defines a rectangle
+"""
+
 
 class Rectangle:
-    """ class defining a rectangle """
+    """
+    class defining a rectangle
+    """
     def __init__(self):
-        """initialize class varianbles"""
+        """
+        initialize class varianbles
+        """
         pass
