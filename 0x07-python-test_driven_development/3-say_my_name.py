7918a26430b90951131dbaa9045a231e917eb9ed
Paul Ogolla <paulotieno2@gmail.com>
2024-02-17 02:24:17 +0300

    redo

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+	""" print names """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
