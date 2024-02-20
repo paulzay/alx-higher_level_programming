06eb96649329a368ede216639416029b587f3dab
Paul Ogolla <paulotieno2@gmail.com>
2024-02-20 12:21:50 +0300

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" define function to write a file"""
+
+
+def write_file(filename="", text=""):
+    """ define function to write string to a file"""
+    with open(filename, "w", encoding="utf-8") as mf:
+        return mf.write(text) 
