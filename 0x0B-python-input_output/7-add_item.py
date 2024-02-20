0c7b6033cf20dc2fe88cb09ca2becc4021eaed31
Paul Ogolla <paulotieno2@gmail.com>
2024-02-20 23:35:56 +0300

    obj

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import os
+import sys
+import json
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+the_list = []
+
+if os.path.exists(filename) and os.path.getsize(filename) > 0:
+    the_list = load_from_json_file(filename)
+
+if len(sys.argv) > 1:
+    for i in sys.argv[:1]:
+        the_list.append(i)
+save_to_json_file(the_list, filename)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+"""define a function"""
+
+
+def class_to_json(obj):
+    """function to return dict description"""
+    return obj.
