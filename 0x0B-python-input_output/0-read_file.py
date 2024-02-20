094a8b04a43d8c166fd26e93f05cb4357e548c43
Paul Ogolla <paulotieno2@gmail.com>
2024-02-20 13:44:47 +0300

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""function to read and print from file"""
+
+
 def read_file(filename=""):
     """ function to read file and print the output """
     with open(filename, mode="r", encoding="utf-8") as MYFILE:
