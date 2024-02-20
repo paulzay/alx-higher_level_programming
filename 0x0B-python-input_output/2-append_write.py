712f7ae51d7320c53586b93e120b71c9d62cb682
Paul Ogolla <paulotieno2@gmail.com>
2024-02-20 14:14:02 +0300

    fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 
 def append_write(filename="", text=""):
     """function to append string at the end"""
-    with open(filename, "a" encoding="utf-8") as mf:
+    with open(filename, "a") as mf:
         return mf.write(text)
