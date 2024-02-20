a7604d6c06b4ef4b3658893024b60c176abbd0a8
Paul Ogolla <paulotieno2@gmail.com>
2024-02-21 00:58:16 +0300

    ???

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,15 +4,12 @@
 
 
 class Student:
-    """student class definition"""
     def __init__(self, first_name, last_name, age)
-        """initialize data"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """do sth"""
         if (type(attrs)) == list and all(type(i) == str for i in attrs):
             return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
         else:
