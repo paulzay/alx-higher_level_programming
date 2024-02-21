45ce562be07a4cd24db4a3b3e369b299667be931
Paul Ogolla <paulotieno2@gmail.com>
2024-02-21 22:54:58 +0300

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""class"""
+
+
+class BaseGeometry:
+    """base geometry"""
+    def __init__(self):
+        """initialize data"""
+        pass
+
+    def area(self):
+        raise Exception("area is not implemented")
