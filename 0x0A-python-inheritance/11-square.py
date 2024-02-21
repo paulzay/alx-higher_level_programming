434936ddb69bc6acb08962d71869459a16804424
Paul Ogolla <paulotieno2@gmail.com>
2024-02-22 01:35:55 +0300

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""class definition"""
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        """initialzie data"""
+        self.__size = size
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+
+    def area(self):
+        """calc area of square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """print square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
