01f55633bb7cbd1471a2c6cb65b83d89245b4344
Paul Ogolla <paulotieno2@gmail.com>
2024-02-22 12:49:29 +0300

    start

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""class definition"""
+
+
+class Node:
+    """Node class defining a linkedlist"""
+    def __init__(self, data, next_node=None):
+        """initialize variables"""
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        self.__data = value
+        if type(self.__data) is not int:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        return self.__data
+
+    @next_node.setter
+    def next_node(self, value):
+        self.__next_node = value
+        if type(self.__next_node) is not None or isinstance(next_node) != Node:
+            raise TypeError("next_node must be a Node object")
+
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def sorted_insert(self, value):
+        pass
