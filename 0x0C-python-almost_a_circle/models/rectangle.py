2decd0658268d64ff44e9a6258008570689cdefa
Paul Ogolla <paulotieno2@gmail.com>
2024-02-26 08:50:27 +0300

    done commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 from models.base import Base
 
+""" define rectangle"""
+
+
 class Rectangle(Base):
+    """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize class values"""
         self.__width = width
         self.__height = height
         self.__x = x
