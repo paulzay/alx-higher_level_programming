d45fb894da389f7505c0ffc43ab540f773311144
Paul Ogolla <paulotieno2@gmail.com>
2024-02-27 14:00:53 +0300

    fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 
 """define rectangle"""
 
-Base = __import__('base').Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -14,94 +14,3 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
-
-    @property
-    def width(self):
-        """get width"""
-        return self.__width
-
-    @property
-    def height(self):
-        """get height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """get x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """get y"""
-        return self.__y
-
-    @height.setter
-    def height(self, value):
-        """set height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @width.setter
-    def width(self, value):
-        """set width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @y.setter
-    def y(self, value):
-        """set y"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    @x.setter
-    def x(self, value):
-        """set x"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    def area(self):
-        """calculate area"""
-        return self.__height * self.__width
-
-    def display(self):
-        """display with #"""
-        if self.__width != 0 and self.__height != 0:
-            return ""
-        s = ""
-        for _ in range(self.__height):
-            s += "#" * self.__width + "\n"
-        return s.rstrip()
-
-    def __str__(self):
-        """representation"""
-        return "[Rectangle] {:d}/{:d} - {:d}/{:d} \
-            ".format(self.__x, self.__y, self.__width, self.__height)
-
-    def update(self, *args, **kwargs):
-        """update function"""
-        if args and len(args) > 0:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
-        else:
-            for j in kwargs:
-                print(j, kwargs[j])
