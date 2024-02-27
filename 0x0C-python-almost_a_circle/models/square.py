0e6e74b71e2770f0f73ed0a1e9b500d51c3c9ae2
Paul Ogolla <paulotieno2@gmail.com>
2024-02-28 00:41:50 +0300

    sdfldfl

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     """square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """initialize data"""
-        super().__init__(size, size, id, x, y)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
@@ -25,3 +25,25 @@ class Square(Rectangle):
         """print"""
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.height)
+
+    def update(self, *args, **kwargs):
+        """update function"""
+        if args and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for j in kwargs:
+                if j == "id":
+                    self.id = kwargs[j]
+                if j == "size":
+                    self.size = kwargs[j]
+                if j == "x":
+                    self.x = kwargs[j]
+                if j == "y":
+                    self.y = kwargs[j]
