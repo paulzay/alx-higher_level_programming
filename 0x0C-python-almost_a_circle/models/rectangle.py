ac9c03589f2337f25b7415eef373af3834079e03
Paul Ogolla <paulotieno2@gmail.com>
2024-02-27 21:38:07 +0300

    add test and fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,8 +86,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """representation"""
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d} \
-            ".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".\
+            format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     # def update(self, *args, **kwargs):
     #     """update function"""
--- 0x0C-python-almost_a_circle/tests/test_rectangular.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangular.py
@@ -5,3 +5,14 @@ class RectangleTest(unittest.TestCase):
 	def test_area(self):
 		rectangle = Rectangle(8, 2)
 		self.assertEqual(rectangle.area(), 16)
+
+	# def test_width_string(self):
+	# 	rectangle = Rectangle("8", 2)
+	# 	self.assertNotEqual()
+
+	# def test_height_string(self):
+	# 	rectangle = Rectangle(8, "2")
+	# 	self.assertEqual
+	def test_str(self):
+		rectangle = Rectangle(4, 6, 2, 1, 12)
+		self.assertEqual(rectangle.__str__(), "[Rectangle] (12) 2/1 - 4/6")
