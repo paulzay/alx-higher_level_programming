0ffd23056933f33f85ae41f6d5faadd5bcc3822d
Paul Ogolla <paulotieno2@gmail.com>
2024-03-01 23:08:21 +0300

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,20 +17,20 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """to json"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
-        return json.dumps(list_dictionaries)
+    # @staticmethod
+    # def to_json_string(list_dictionaries):
+    #     """to json"""
+    #     if list_dictionaries is None or list_dictionaries == []:
+    #         return "[]"
+    #     return json.dumps(list_dictionaries)
 
     # def save_to_file(cls, list_objs):
     #     """save to file"""
     #     pass
 
-    def from_json_string(json_string):
-        """from json"""
-        return json.loads(json_string)
+    # def from_json_string(json_string):
+    #     """from json"""
+    #     return json.loads(json_string)
 
     # def create(cls, **dictionary):
     #     """create"""
