8311e006bb76009f727150a9de09375460339d71
Paul Ogolla <paulotieno2@gmail.com>
2024-03-27 01:03:57 +0300

    done

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- group
+SELECT score,
+	COUNT(*) as `number`
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- no link
+SELECT score,
+	name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
