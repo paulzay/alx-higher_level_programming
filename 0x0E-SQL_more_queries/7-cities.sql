0f34cebc18eec91a668aae6142264713ce7e41a7
Paul Ogolla <paulotieno2@gmail.com>
2024-03-27 22:42:37 +0300

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  state_id INT FOREIGN KEY REFERENCES states(state_id),
+  state_id INT FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
   name VARCHAR(256) NOT NULL,
 );
