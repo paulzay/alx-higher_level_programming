9d8b41f913b1bd37bc6372aa913d4080e25f957b
Paul Ogolla <paulotieno2@gmail.com>
2024-03-27 19:42:42 +0300

    2 done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
--- create table and user grant select access
+-- create user and table, granted select access
 CREATE TABLE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
