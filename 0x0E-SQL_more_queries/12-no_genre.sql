3b13853bd76bb62ed4dd6a89f78de0b6c21252bf
Paul Ogolla <paulotieno2@gmail.com>
2024-03-28 22:43:11 +0300

    fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- no genre
 SELECT tv_shows.title, tv_show_genres.genre_id
         FROM tv_shows_genres
-        RIGHT JOIN tv_shows.id = tv_show_genres.show_id
+        RIGHT JOIN tv_shows
+        WHERE show.id = tv_show_genres.show_id
         ON tv_shows.genre_id IS NULL
         ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
