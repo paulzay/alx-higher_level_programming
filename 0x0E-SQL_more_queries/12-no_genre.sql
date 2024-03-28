8520e53a69eaa52159d23a21d4c0616cf838595a
Paul Ogolla <paulotieno2@gmail.com>
2024-03-28 15:55:32 +0300

    12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,6 @@
--- comment
+-- no genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+        FROM tv_shows_genres
+        FULL JOIN tv_shows
+        ON tv_shows.genre_id IS NULL
+        ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
