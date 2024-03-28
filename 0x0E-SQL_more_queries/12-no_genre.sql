6fbd2f92ab7354c83c178b6f34387ac90cae18e7
Paul Ogolla <paulotieno2@gmail.com>
2024-03-28 22:46:04 +0300

    12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
         FROM tv_shows_genres
         RIGHT JOIN tv_shows
-        WHERE show.id = tv_show_genres.show_id
-        ON tv_shows.genre_id IS NULL
+        ON tv_shows.id = tv_show_genres.show_id
+        WHERE tv_show_genres IS NULL
         ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
