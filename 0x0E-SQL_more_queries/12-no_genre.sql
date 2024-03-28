c35242c0a6813736bc7e2addae008718bb1cd4e1
Paul Ogolla <paulotieno2@gmail.com>
2024-03-28 22:48:23 +0300

    2welf

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,8 @@
--- no genre
+-- null
 SELECT tv_shows.title, tv_show_genres.genre_id
-        FROM tv_shows_genres
-        RIGHT JOIN tv_shows
-        ON tv_shows.id = tv_show_genres.show_id
-        WHERE tv_show_genres IS NULL
-        ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+      FROM tv_shows_genres
+      RIGHT JOIN tv_shows
+      ON tv_shows.id = tv_show_genres.show_id
+      WHERE tv_show_genres IS NULL
+      ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+
