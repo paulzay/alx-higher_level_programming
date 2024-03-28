3f3706f247703aa9595f5850f9aee51c362c9e04
Paul Ogolla <paulotieno2@gmail.com>
2024-03-28 10:26:55 +0300

    88

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list cities
 SELECT id, name 
-      FROM hbtn_0d_usa 
-      WHERE name = 'California'
-      ORDER BY hbtn_0d_usa.cities.id ASC;
+      FROM cities; 
+      WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+      ORDER BY id ASC;
