1772d6290256cf9655533118557ab2bafd0f4fc7
Paul Ogolla <paulotieno2@gmail.com>
2024-03-28 13:15:40 +0300

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- genre id by show
 
-SELECT tv_shows.title, tv_show_genres.genre.id
+SELECT tv_shows.title, tv_show_genres.genre_id
       FROM tv_show_genres
       INNER JOIN tv_shows
       ON tv_shows.id = tv_show_genres.show_id
