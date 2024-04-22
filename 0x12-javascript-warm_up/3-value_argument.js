ade69537e85433b832a89ac6f6b2313a4aff7e23
Paul Ogolla <paulotieno2@gmail.com>
2024-04-22 22:45:34 +0300

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,6 +5,5 @@ const { argv } = require('node:process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  const arg2 = argv[2];
-  console.log(arg2);
+  console.log(argv[2]);
 }
