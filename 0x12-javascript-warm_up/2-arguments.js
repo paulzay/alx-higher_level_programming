fb81117f854907cb4f07d3a969287df224c44751
Paul Ogolla <paulotieno2@gmail.com>
2024-04-22 18:32:11 +0300

    2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length == 1) {
+  console.log('No argument');
+} else if (argv.length == 2) {
+  console.log('Argument found');
+} else if (argv.length > 2) {
+  console.log('Arguments found');
+}
