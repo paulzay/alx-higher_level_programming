8a19bbc577ef26a441fc36df4994428c6d2b2ec8
Paul Ogolla <paulotieno2@gmail.com>
2024-04-23 12:20:33 +0300

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const arg = parseInt(process.argv[2], 10);
 
-if (isNaN(parseInt(argv[2], 10))) {
-  console.log('My number: ' + parseInt(argv[2], 10));
-} else {
+if (isNaN(arg)) {
   console.log('Not a number');
+} else {
+  console.log(`My number: ${arg}`);
 }
