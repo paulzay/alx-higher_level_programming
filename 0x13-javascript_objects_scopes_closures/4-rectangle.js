0b08a5f116a4c13a2d2a2235710a981b11f36fa6
Paul Ogolla <paulotieno2@gmail.com>
2024-04-23 23:24:32 +0300

    4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i += 1) {
+      let row = '';
+      for (let j = 0; j < this.width; j += 1) row += 'X';
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    return (this.height * 2, this.width * 2);
+  }
+}
+
+module.exports = Rectangle;
