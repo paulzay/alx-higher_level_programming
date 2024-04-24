f78c3a2a2aef2ea805a21093b04791a2d3377f5f
Paul Ogolla <paulotieno2@gmail.com>
2024-04-24 09:37:53 +0300

    5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+Rectangle;
+class Square extends Rectangle {
+  constructor(size) {
+    super(w, h);
+    this.w = size;
+  }
+}
+
+module.exports = Square;
