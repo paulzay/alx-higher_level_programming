f2a7cfc006fcad4ad3401e1d2eb0047240251923
Paul Ogolla <paulotieno2@gmail.com>
2024-04-24 09:25:56 +0300

    3,4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i += 1) {
       let row = '';
       for (let j = 0; j < this.width; j += 1) row += 'X';
