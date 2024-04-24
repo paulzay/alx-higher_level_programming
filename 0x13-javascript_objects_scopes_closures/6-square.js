a31688c2e32c695ae1870dee10b93d68cb24e70b
Paul Ogolla <paulotieno2@gmail.com>
2024-04-24 13:07:39 +0300

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,9 @@
 const Sq = require('./5-square');
 
 class Square extends Sq {
-  charPrint(c) {
+  charPrint (c) {
     for (let i = 0; i < this.height; i++) {
-      const row = '';
+      let row = '';
       for (let j = 0; j < this.width; j++) row += c || 'X';
       console.log(row);
     }
