839c579853d8b53085c3330b709012f6a4c2e99a
Paul Ogolla <paulotieno2@gmail.com>
2024-04-24 13:32:19 +0300

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js');
-const arr = [];
-list.map((num, index) => {
-  arr.push(num * index);
-});
+
+const arr = list.map((num, index) => num * index);
+
 console.log(list);
 console.log(arr);
