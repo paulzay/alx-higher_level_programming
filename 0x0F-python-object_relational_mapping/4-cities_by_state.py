20222c6913891c11f6cd30689e4e8a74b196386a
Paul Ogolla <paulotieno2@gmail.com>
2024-04-29 14:46:37 +0300

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+""" comment """
+import sys
+import MySQLdb
+
+
+def main():
+    argv = sys.argv
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name, states.name FROM cities \
+            INNER JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
