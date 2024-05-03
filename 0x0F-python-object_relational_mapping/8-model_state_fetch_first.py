85bb8d72d8f454fcbd1dc8b2d0a8dddb431022d6
Paul Ogolla <paulotieno2@gmail.com>
2024-05-04 00:19:28 +0300

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""comment"""
+from sqlalchemy import engine
+import sys
+from model_state import Base, State
+
+
+def main():
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    data = session.query(State).order_by(State.id).first()
+    if data:
+        print("{}: {}".format(data.id, data.name))
+
+    session.close()
+
+
+if __name__ == '__main__':
+    main()
