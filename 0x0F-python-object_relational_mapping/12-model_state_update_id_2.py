a9830da5f90dfa8938c4d0ba64a9efc06fe95b27
Paul Ogolla <paulotieno2@gmail.com>
2024-05-04 01:23:55 +0300

    12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""comment"""
+import sys
+from sqlalchemy import engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+def main():
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    main()
