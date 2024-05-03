34d5b89a91c1968494ce0739ddd17485db274437
Paul Ogolla <paulotieno2@gmail.com>
2024-05-04 01:01:16 +0300

    insert

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""commment"""
+import sys
+
+
+def main():
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    state = session.query(State).filter(State.name == 'Louisiana').first()
+    if state:
+        print("{}".format(state.id))
+
+
+if __name__ == '__main__':
+    main()
