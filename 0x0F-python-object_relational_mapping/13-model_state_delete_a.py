77bffad3395a18a164b33ec0d2cc934309e5452d
Paul Ogolla <paulotieno2@gmail.com>
2024-05-04 01:27:12 +0300

    13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+"""comment"""
+import sys
+from sqlalchemy import engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+def main():
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(state)
+    session.close()
+
+
+if __name__ == '__main__':
+    main()
