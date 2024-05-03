effccd4e97dcea9b9e9ff5ea1c10d69b4deeb4dd
Paul Ogolla <paulotieno2@gmail.com>
2024-05-04 01:44:10 +0300

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""comment"""
+import sys
+from sqlalchemy import engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+def main():
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    main()
