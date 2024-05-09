ba60b3d2ba13f24f5a0d6498d26e3be681a8ba99
Paul Ogolla <paulotieno2@gmail.com>
2024-05-09 21:44:46 +0300

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 # cURL body size
-import sys
-argv = sys.argv
 
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}' 
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# cURL to the end
+
+curl -s 
