7da179e3ddeac23089c1e290921d0a7d1de941dc
Paul Ogolla <paulotieno2@gmail.com>
2024-05-09 23:50:58 +0300

    7

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  cURL POST parametes
-curl -s "$1" -X POST -d "test@gmail.com" -d "subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
