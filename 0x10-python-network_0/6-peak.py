53da20a32cc1df1292a2a00d73b1e6a716380b43
Paul Ogolla <paulotieno2@gmail.com>
2024-05-09 23:39:34 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# find peak
+"""find the peak from list"""
 
 
 def find_peak(list_of_integers):
     if len(list_of_integers) < 1:
         return None
     return max(list_of_integers)
+
 
 if __name__ == "__main__":
     find_peak()
