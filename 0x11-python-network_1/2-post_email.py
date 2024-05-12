582b26bc34202d9bd83eb4bbd8d6c0eeedfa8da7
Paul Ogolla <paulotieno2@gmail.com>
2024-05-12 18:58:34 +0300

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ def main():
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-'", 'replace'))
+        print(response.read().decode('utf-8', 'replace'))
 
 
 if __name__ == "__main__":
