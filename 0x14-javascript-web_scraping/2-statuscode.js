5453709e654f49052da6e416d13a86d23990669e
Paul Ogolla <paulotieno2@gmail.com>
2024-06-04 22:58:21 +0300

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const request = require('request');
 const { argv } = require('node:process');
 
 request(argv[2], function (error, response, body) {
-  console.error('error:', error);
-  console.log('code:', response && response.statusCode);
+  console.error('error: ', error);
+  console.log('code: ', response && response.statusCode);
 });
