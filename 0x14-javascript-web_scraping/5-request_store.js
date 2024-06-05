4c60b15b32830341f445a7a606261e490e3b6097
Paul Ogolla <paulotieno2@gmail.com>
2024-06-05 05:56:25 +0300

    fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,14 +5,14 @@ const { argv } = require('node:process');
 const fs = require('fs');
 
 request(argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  try {
-  	fs.writeFile(process.argv[3], body, 'utf8', function (err, result) {
-  		if (err) console.log(err);
-  	});
-  } catch (err) {
-  	console.log(err);
-  }
+	if (error) {
+		console.error(error);
+	}
+	try {
+		fs.writeFile(process.argv[3], body, 'utf8', function (err, result) {
+			if (err) console.log(err);
+	});
+	} catch (err) {
+		console.log(err);
+	}
 });
